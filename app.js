88564c4e680398ac5e99d219f64de304b71ac32b
handycodez <ahanug28@gmail.com>
2023-04-26 14:25:01 +0700

    add cors

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
+app.use(cors());
 app.use(require("./routes/api"));
 
 app.get("/", (req, res) => {
@@ -14,7 +16,7 @@ app.get("/", (req, res) => {
   });
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`SERVER RUNNING AT PORT ${port} WITHOUT YOU`);
 });
