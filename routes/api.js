900377b265f7c5826396070235a0e0320df29d62
handycodez <ahanug28@gmail.com>
2023-04-27 13:01:00 +0700

    add search

--- routes/api.js
+++ routes/api.js
@@ -5,9 +5,11 @@ const {
   comicList,
   comicdetail,
   comicchapter,
+  search,
 } = require("../controllers/apiController");
 router.get("/api/comic-list/:page", comicList);
 router.get("/api/comic-detail/:endpoint", comicdetail);
 router.get("/api/comic-chapter/:endpoint", comicchapter);
+router.get("/api/comic-search/:query", search);
 
 module.exports = router;
